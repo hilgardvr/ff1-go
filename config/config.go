0fa05f26aebef9a6d397ba98e03bb1eda7312db1
Hilgard van Rooyen <hilgardvr@gmail.com>
2023-01-23 07:51:07 +0200

    Feat: Read env config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,9 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Config struct {
@@ -18,12 +21,37 @@ type Config struct {
 func ReadConfig(path string) (*Config, error) {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return readEnvConfig()
+	} else {
+		config := Config{}
+		err = json.Unmarshal(file, &config)
+		if err != nil {
+			return nil, err
+		}
+		return &config, nil
 	}
+}
+
+func readEnvConfig() (*Config, error) {
 	config := Config{}
-	err = json.Unmarshal(file, &config)
-	if err != nil {
-		return nil, err
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		switch pair[0] {
+			case "APP_PORT": config.AppPort = pair[1]
+			case "NEO4J_URI": config.Neo4jUri = pair[1]
+			case "NEO4J_USERNAME": config.Neo4jUsername = pair[1]
+			case "NEO4J_PASSWORD": config.Neo4jPassword = pair[1]
+			case "APP_PASSWORD": config.EmailPassword = pair[1]
+			case "EMAIL_FROM": config.EmailFrom = pair[1]
+			case "SEND_EMAILS": {
+				toggle, err := strconv.ParseBool(pair[1])
+				if err != nil {
+					return &config, err
+				}
+				config.SendEmails = toggle
+			}
+		default: 
+		}
 	}
 	return &config, nil
 }
